ce1e9e5cb3464bbc9f833d74754e07d034783190
Juan Camilo Cardona Calderon <juan.cardona69@udea.edu.co>
2021-07-19 11:21:31 -0500

    8. Inyeccion de dependencias - Java Annotations - Constructor Inyection - practica terminada

---
+++ Inyeccion-de-Dependencias-5/src/dev/j3c/Contador.java
@@ -0,0 +1,51 @@
+package dev.j3c;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component("miContador")
+public class Contador implements Empleado {
+
+	private Reporte reporte;
+	
+	public Contador() {}
+	
+	@Autowired
+	public Contador(Reporte reporte) {
+		//System.out.println("Inyectando una referencia de la clase Reporte en el constructor...");
+		this.reporte = reporte;
+	}
+	
+	public Reporte getReporte() {
+		return reporte;
+	}
+
+	public void setReporte(Reporte reporte) {
+		this.reporte = reporte;
+	}
+
+	@Override
+	public void registrarLlegada() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void iniciarLabores() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String describirActividades() {
+		// TODO Auto-generated method stub
+		return ("Soy el contador, controlo los estados financieros, y realizo transacciones contables.");
+	}
+
+	@Override
+	public void registrarSalida() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ Inyeccion-de-Dependencias-5/src/dev/j3c/Prueba.java
@@ -0,0 +1,18 @@
+package dev.j3c;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Prueba {
+	public static void main(String[] args) {
+	ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+	
+	Empleado empleado = context.getBean("miGerente", Empleado.class);
+	
+	System.out.println(empleado.describirActividades());
+	System.out.println(empleado.getReporte().getTitulo());
+	System.out.println(empleado.getReporte().getDescripcion());
+	
+	context.close();
+	
+	}
+}
