c99b602fc5b35285a2a120a3301ec069db2d8be5
Juan Camilo Cardona Calderon <juan.cardona69@udea.edu.co>
2021-07-19 12:11:36 -0500

    11. Inyeccion de dependencias - Java Annotations - Default Bean Id - Setter Inyection - practica terminada

---
+++ Inyeccion-de-Dependencias-8/src/dev/j3c/Contador.java
@@ -0,0 +1,47 @@
+package dev.j3c;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Contador implements Empleado {
+
+	private Reporte reporte;
+	
+	public Contador() {}
+	
+	public Reporte getReporte() {
+		return reporte;
+	}
+
+	@Autowired
+	public void setReporte(Reporte reporte) {
+		//System.out.println("Inyectando una referencia de Reporte por medio de metodo setter...");
+		this.reporte = reporte;
+	}
+
+	@Override
+	public void registrarLlegada() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void iniciarLabores() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String describirActividades() {
+		// TODO Auto-generated method stub
+		return ("Soy el contador, controlo los estados financieros, y realizo transacciones contables.");
+	}
+
+	@Override
+	public void registrarSalida() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ Inyeccion-de-Dependencias-8/src/dev/j3c/Gerente.java
@@ -0,0 +1,47 @@
+package dev.j3c;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Gerente implements Empleado {
+
+	private Reporte reporte;
+	
+	public Gerente() {}
+	
+	public Reporte getReporte() {
+		return reporte;
+	}
+	
+	@Autowired
+	public void setReporte(Reporte reporte) {
+		System.out.println("Inyectando una referencia de Reporte por medio de metodo setter...");
+		this.reporte = reporte;
+	}
+	
+	@Override
+	public void registrarLlegada() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void iniciarLabores() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String describirActividades() {
+		// TODO Auto-generated method stub
+		return ("Soy el gerente, planifico, organizo y coordino el trabajo de la empresa");
+	}
+
+	@Override
+	public void registrarSalida() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
