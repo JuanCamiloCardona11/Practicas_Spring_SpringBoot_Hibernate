1e1faa2b42eb49384818276d7cc345ab2beeac3c
Juan Camilo Cardona Calderon <juan.cardona69@udea.edu.co>
2021-07-19 12:06:07 -0500

    10. Inyeccion de dependencias - Java Annotations - Setter Inyection - practica terminada

---
+++ Inyeccion-de-Dependencias-7/src/dev/j3c/Secretario.java
@@ -0,0 +1,47 @@
+package dev.j3c;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component("miSecretario")
+public class Secretario implements Empleado{
+
+	private Reporte reporte;
+	
+	public Secretario() {}
+	
+	public Reporte getReporte() {
+		return reporte;
+	}
+	
+	@Autowired
+	public void setReporte(Reporte reporte) {
+		//System.out.println("Inyectando referencia de Reporte por medio de metodo setter");
+		this.reporte = reporte;
+	}
+	
+	@Override
+	public void registrarLlegada() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void iniciarLabores() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String describirActividades() {
+		// TODO Auto-generated method stub
+		return ("Soy el secretario, recibo documentos, atiendo llamadas telefónicas y atiendo visitas");
+	}
+
+	@Override
+	public void registrarSalida() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
