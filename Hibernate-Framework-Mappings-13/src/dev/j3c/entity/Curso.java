9163e62d8f34c79cdfa0cfb8dc011e6e0ffe07eb
Juan Camilo Cardona Calderon <juan.cardona69@udea.edu.co>
2021-07-24 21:10:49 -0500

    55. Hibernate Framework - Mappings  - @ManyToMany - bidireccional - Operacion: Agregando estudiantes a un curso

---
+++ Hibernate-Framework-Mappings-13/src/dev/j3c/entity/Curso.java
@@ -0,0 +1,134 @@
+package dev.j3c.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="curso")
+public class Curso {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(name="titulo")
+	private String titulo;
+	
+	@Column(name="descripcion")
+	private String descripcion;
+	
+	@ManyToOne(cascade={ CascadeType.PERSIST, CascadeType.MERGE,
+						 CascadeType.DETACH, CascadeType.REFRESH
+	})
+	@JoinColumn(name="profesor_id")
+	private Profesor profesor;
+	
+	@OneToMany(fetch=FetchType.LAZY, cascade=CascadeType.ALL)
+	@JoinColumn(name="curso_id")
+	private List<RevisionCurso> listaRevisiones;
+	
+	@ManyToMany(fetch=FetchType.LAZY,cascade= {
+			CascadeType.DETACH,CascadeType.MERGE,
+			CascadeType.PERSIST,CascadeType.REFRESH})
+	@JoinTable(name="curso_estudiante",
+			   joinColumns=@JoinColumn(name="curso_id"),
+			   inverseJoinColumns=@JoinColumn(name="estudiante_id"))
+	private List<Estudiante> listaEstudiantes;
+	
+	public Curso() {
+	}
+
+	public Curso(String titulo, String descripcion) {
+		this.titulo = titulo;
+		this.descripcion = descripcion;
+		this.profesor = null;
+	}
+	
+	public Curso(int id, String titulo, String descripcion) {
+		this.id = id;
+		this.titulo = titulo;
+		this.descripcion = descripcion;
+		this.profesor = null;
+	}
+	
+	public void agregarRevisionCurso(RevisionCurso revisionCurso) {
+		if(this.listaRevisiones == null) {
+			this.listaRevisiones = new ArrayList<>();
+		}
+		this.listaRevisiones.add(revisionCurso);
+	}
+
+	public void agregarEstudiante(Estudiante estudiante) {
+		if(this.listaEstudiantes == null) {
+			this.listaEstudiantes = new ArrayList<>();
+		}
+		this.listaEstudiantes.add(estudiante);
+	}
+	
+	public int getId() {
+		return this.id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return this.titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getDescripcion() {
+		return this.descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public Profesor getProfesor() {
+		return this.profesor;
+	}
+
+	public void setProfesor(Profesor profesor) {
+		this.profesor = profesor;
+	}
+	
+	public List<RevisionCurso> getListaRevisiones() {
+		return this.listaRevisiones;
+	}
+
+	public void setListaRevisiones(List<RevisionCurso> listaRevisiones) {
+		this.listaRevisiones = listaRevisiones;
+	}
+	
+	public List<Estudiante> getListaEstudiantes() {
+		return this.listaEstudiantes;
+	}
+
+	public void setListaEstudiantes(List<Estudiante> listaEstudiantes) {
+		this.listaEstudiantes = listaEstudiantes;
+	}
+	
+	@Override
+	public String toString() {
+		return "Curso [id=" + this.id + ", titulo=" + this.titulo + ", descripcion=" + this.descripcion + "]";
+	}	
+}
---
+++ Hibernate-Framework-Mappings-13/src/dev/j3c/test/Prueba.java
@@ -0,0 +1,62 @@
+package dev.j3c.test;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import dev.j3c.entity.*;
+
+public class Prueba {
+	
+	public static void main(String[] args) {
+		SessionFactory factory = new Configuration()
+				.configure("hibernate.cfg.xml")
+				.addAnnotatedClass(Profesor.class)
+				.addAnnotatedClass(DetalleProfesor.class)
+				.addAnnotatedClass(Curso.class)
+				.addAnnotatedClass(RevisionCurso.class)
+				.addAnnotatedClass(Estudiante.class)
+				.buildSessionFactory();
+		Session sesion = factory.getCurrentSession();
+		try {
+			sesion.beginTransaction();
+			
+			Curso curso1 = new Curso("Curso avanzado de Python","En este curso aprenderas analisis de datos con python");
+			Curso curso2 = new Curso("Curso de programacion web HTML, CSS y JS","En este curso aprenderas los fundamentos de programacion web");
+			Curso curso3 = new Curso("Curso de programacion orientada a objetos","En este curso aprenderas los fundamentos de POO");
+			
+			Estudiante estudiante1 = new Estudiante("Pedro","Valencia","pedro@gmail.com");
+			Estudiante estudiante2 = new Estudiante("Luis","Quiceno","luis12@gmail.com");
+			Estudiante estudiante3 = new Estudiante("Andrea","Lopez","andre-lopez@gmail.com");
+			Estudiante estudiante4 = new Estudiante("Ana","Carmona","ana-carmona2@gmail.com");
+			
+			sesion.save(curso1);
+			sesion.save(curso2);
+			sesion.save(curso3);
+			
+			curso1.agregarEstudiante(estudiante1);
+			curso1.agregarEstudiante(estudiante2);
+			curso1.agregarEstudiante(estudiante4);
+			
+			curso2.agregarEstudiante(estudiante1);
+			curso2.agregarEstudiante(estudiante2);
+			curso2.agregarEstudiante(estudiante3);
+			
+			curso3.agregarEstudiante(estudiante2);
+			curso3.agregarEstudiante(estudiante4);
+			
+			sesion.save(estudiante1);
+			sesion.save(estudiante2);
+			sesion.save(estudiante3);
+			sesion.save(estudiante4);
+			
+			sesion.getTransaction().commit();
+		} catch (Exception e) {
+			e.printStackTrace(System.out);
+			sesion.getTransaction().rollback();
+		} finally { 
+			sesion.close();
+			factory.close();
+		}
+	}
+}
