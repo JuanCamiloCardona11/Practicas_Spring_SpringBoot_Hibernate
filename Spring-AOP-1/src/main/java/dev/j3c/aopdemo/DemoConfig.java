7e91495ca3e73dabd590096b3e10d7fe01890106
Juan Camilo Cardona Calderon <juan.cardona69@udea.edu.co>
2021-08-04 21:25:55 -0500

    59. Spring AOP - @Before - Demo App

---
+++ Spring-AOP-1/src/main/java/dev/j3c/aopdemo/DemoConfig.java
@@ -0,0 +1,11 @@
+package dev.j3c.aopdemo;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+@ComponentScan("dev.j3c.aopdemo")
+public class DemoConfig {
+}
